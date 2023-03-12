6a1add29c5b7edef6f1a50122dcf6e4d3a9f0078
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-03-12 23:53:51 +0200

    Second lesson

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+'use strict';
+
+a = 15;
+console.log(a);
+
+//number = 44;
 let number = 5;
 const leftBorderWidth = 1;
 
@@ -5,3 +11,35 @@ number = 10;
 console.log(number);
 
 //leftBorderWidth = 12;  //mistake
+
+const obj = {
+    a: 50,
+};
+
+obj.a = 'hahaha';
+console.log(obj);
+
+name = 'Alex';  // hosting
+var name = 'Vasek';
+
+{
+    //let result = 55;
+    var result = 22;
+}
+
+console.log(result);
+
+//alert(22)
+//[].push(12)
+
+//==========================================================================================================
+
+const a = 5000;
+const b = 4000;
+
+console.log('Ширина кузова автомобиля: ' + a + ', a висота кузова автомобиля: ' + b);
+
+const carBodyWidth =  5000;
+const carBodyLength = 4000;
+
+console.log('Ширина кузова автомобиля: ' + carBodyWidth + ', a висота кузова автомобиля: ' + carBodyLength);
