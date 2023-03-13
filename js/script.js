70565ee235a9187d8fe471e6eaf8d808b39e9a20
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-03-13 15:42:31 +0200

    Finish bigInt variables

--- js/script.js
+++ js/script.js
@@ -43,3 +43,38 @@ const carBodyWidth =  5000;
 const carBodyLength = 4000;
 
 console.log('Ширина кузова автомобиля: ' + carBodyWidth + ', a висота кузова автомобиля: ' + carBodyLength);
+
+// Request
+// data
+// response
+
+//camelCase
+//snake_case
+//UPPER_SNAKE_CASE 
+// Kebab-case 
+// PascalCase
+
+let COLOR_RED = '#F00';
+const _apiBase = 'https://gateway/marvel.443/v1/public/'; // не менять
+
+//=====================================================================================
+
+const max = Number.MAX_SAFE_INTEGER;
+// → 9007199254740991
+
+BigInt(Number.MAX_SAFE_INTEGER) + 2n;
+// → 9_007_199_254_740_993n
+
+(7 + 6 - 5) * 4 ** 3 / 2 % 3;
+// → 1
+(7n + 6n - 5n) * 4n ** 3n / 2n % 3n;
+// → 1n
+
+BigInt(123);
+// → 123n
+BigInt(1.5);
+// → RangeError
+BigInt('1.5');
+// → SyntaxError
+
+//===============================================================================
