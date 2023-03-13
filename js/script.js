d3c467c59b0d6fcf6c443ac75682889df37e6efb
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-03-13 17:38:46 +0200

    Type of variables

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 'use strict';
 
-a = 15;
-console.log(a);
+// a = 15;
+// console.log(a);
 
 //number = 44;
-let number = 5;
-const leftBorderWidth = 1;
+// let number = 5;
+// const leftBorderWidth = 1;
 
-number = 10;
-console.log(number);
+// number = 10;
+// console.log(number);
 
 //leftBorderWidth = 12;  //mistake
 
-const obj = {
-    a: 50,
-};
+// const obj = {
+//     a: 50,
+// };
 
-obj.a = 'hahaha';
-console.log(obj);
+// obj.a = 'hahaha';
+// console.log(obj);
 
-name = 'Alex';  // hosting
-var name = 'Vasek';
+// name = 'Alex';  // hosting
+// var name = 'Vasek';
 
-{
-    //let result = 55;
-    var result = 22;
-}
+// {
+//     //let result = 55;
+//     var result = 22;
+// }
 
-console.log(result);
+// console.log(result);
 
 //alert(22)
 //[].push(12)
@@ -54,16 +54,16 @@ console.log('Ширина кузова автомобиля: ' + carBodyWidth + 
 // Kebab-case 
 // PascalCase
 
-let COLOR_RED = '#F00';
-const _apiBase = 'https://gateway/marvel.443/v1/public/'; // не менять
+// let COLOR_RED = '#F00';
+// const _apiBase = 'https://gateway/marvel.443/v1/public/'; // не менять
 
 //=====================================================================================
 
-const max = Number.MAX_SAFE_INTEGER;
-// → 9007199254740991
+// const max = Number.MAX_SAFE_INTEGER;
+// // → 9007199254740991
 
-BigInt(Number.MAX_SAFE_INTEGER) + 2n;
-// → 9_007_199_254_740_993n
+// BigInt(Number.MAX_SAFE_INTEGER) + 2n;
+// // → 9_007_199_254_740_993n
 
 (7 + 6 - 5) * 4 ** 3 / 2 % 3;
 // → 1
@@ -78,3 +78,27 @@ BigInt('1.5');
 // → SyntaxError
 
 //===============================================================================
+
+// let number = 9.887;
+console.log(4 / 0);  // Infinity
+console.log(-3 / 0); // -Infinity
+console.log('first' * 2); // NaN
+
+let und;
+console.log(und); // undefined
+// console.log(first);  // ReferenceError: first is not defined
+
+const person = {
+    name: 'Lida',
+    age: 33,
+    isMerried: false,
+};
+
+// console.log(person.name);  // Lida
+// console.log(person[name]);  // ReferenceError: name is not defined
+console.log(person['name']);  // Lida
+
+
+const arr = ['plum.jpg', 6, 'orange.bmp', [2, 55], 'apple.png', {a: 'string', v: 'zero'}];
+console.log(arr[3][1]);  // 55
+console.log(typeof(arr)); // object
