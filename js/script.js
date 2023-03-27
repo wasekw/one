b00e3293501197258d4b77391c945e137805758f
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-03-27 23:31:53 +0300

    Starting second project work with webpage

--- js/script.js
+++ js/script.js
@@ -1,42 +1,26 @@
-const box = document.getElementById('box');
-const btns = document.getElementsByTagName('button');
-const circles = document.getElementsByClassName('circle');
-const wrapper = document.querySelector('.wrapper');
-const hearts = document.querySelectorAll('.heart');
+/* Задания на урок:
 
-// box.style.backgroundColor = '#ff00ff';
-// box.style.width = '500px';
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
 
-btns[1].style.borderRadius = '95%';
-circles[0].style.backgroundColor = 'red';
-box.style.cssText = 'background-color: #ff00ff; width: 400px';
+2) Изменить жанр фильма, поменять "комедия" на "драма"
 
-//for (let i = 0; i < hearts.length; i++) {
-//  hearts[i].style.cssText = 'background-color: green; width: 200px';
-//}
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
 
-hearts.forEach(el => {
-  el.style.cssText = 'background-color: green; width: 140px';
-});
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
 
-const div = document.createElement('div');
-//const text = document.createTextNode('I was here!');
+5) Добавить нумерацию выведенных фильмов */
 
-div.classList.add('black');
-//document.body.append(div);
-document.body.appendChild(div);
-//wrapper.prepend(div);
-//hearts[2].before(div);
-//btns[1].after(div);
+'use strict';
 
-//circles[1].remove();
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
 
-//btns[3].replaceWith(circles[1]);
-//wrapper.insertBefore(div, btns[0]);
-
-//div.innerHTML = 'Hello world!';
-div.innerHTML = '<h1>Hello World!!!</h1>';
-
-//div.textContent = 'HI it`s me!';
-
-div.insertAdjacentHTML('beforeend', '<h2>Hello!!! Baby</h2>');
