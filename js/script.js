3deb1fdc450ef479e941886c6ddf32e34cba3b14
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-03-27 18:03:50 +0300

    Bigin first task

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const box = document.getElementById('box');
+const btns = document.getElementsByTagName('button');
+const circles = document.getElementsByClassName('circle');
+const wrapper = document.querySelector('.wrapper');
+const hearts = document.querySelectorAll('.heart');
+
+// box.style.backgroundColor = '#ff00ff';
+// box.style.width = '500px';
+
+btns[1].style.borderRadius = '95%';
+circles[0].style.backgroundColor = 'red';
+box.style.cssText = 'background-color: #ff00ff; width: 400px';
+
