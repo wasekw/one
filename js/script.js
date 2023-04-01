b02bff0e5c953999ef970bcd263800e0dbf5565f
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-04-01 16:31:54 +0300

    Ending work with mobile events

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 'use strict';
+// touchstart
+// touchmove
+// touchend
+// touchenter
+// touchleave
+// touchcancel
+
+window.addEventListener('DOMContentLoaded', () => {
+   const box = document.querySelector('.box');
+   
+   box.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+
+    //console.log('Start');
+    //console.log(e.touches);
+    console.log(e.targetTouches);
+    console.log(e.changedTouches);
+   })
+//    box.addEventListener('touchmove', (e) => {
+//        e.preventDefault();
+    
+//        console.log('Move');
+//       })
+
+//   box.addEventListener('touchend', (e) => {
+//    e.preventDefault();
+
+//    console.log('End');
+//   })
+
+})
+
+//console.log('hi');
+// touches
+// targetTouches
+// changedTouches
