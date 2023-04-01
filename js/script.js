1b7a9eb208f994695f9527541ed7bb4120fdec04
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-04-01 20:39:46 +0300

    Working with Symbol

--- js/script.js
+++ js/script.js
@@ -86,3 +86,44 @@
 //console.log(Array.from(boxesElement));
 
 //=================================================================
+
+//let id = Symbol('id');
+
+//const obj = {
+//   'name': 'Test',
+//   [id]: 1,
+//   getId: function() {
+//      return this[id];
+//   }
+//}
+
+//let id = Symbol('id');
+//let id2 = Symbol('id');
+//console.log(id == id2); // false
+
+//obj[id] = 1;
+
+//console.log(obj);
+//console.log(obj.getId());
+//console.log(Object.getOwnPropertySymbols(obj));
+//console.log(obj[Object.getOwnPropertySymbols(obj)[0]]);
+
+//for (let value in obj) console.log(value);
+
+//const myAwesomeDB = {
+//   films: [],
+//   actors: [],
+//   //id: 123
+//   //[Symbol('id')]: 123
+//   [Symbol.for('id')]: 123
+//}
+
+//// Сторонний код из другой библиотеки
+
+//myAwesomeDB.id = '1244234524';
+
+//console.log(myAwesomeDB[Symbol.for('id')]);
+//console.log(myAwesomeDB);
+
+//========== Descriptory =====================================
+
