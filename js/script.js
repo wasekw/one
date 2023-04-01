27b40440d54a32f409534fc8e53781e225c69180
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-04-01 18:52:42 +0300

    Ending work with operator optional chaining

--- js/script.js
+++ js/script.js
@@ -21,3 +21,41 @@
 //console.log(userName ?? userKey ?? 'User');
 
 //=================================================
+
+//let temp = obj.first;
+//let nestedProp = ((temp === null || temp === undefined) ? undefined : temp.second);
+
+//let nestedProp = obj.first?.second;
+
+//const box = document.querySelector('.box');
+//const block = document.querySelector('.block');
+
+//console.log(block);
+
+//console.log(block?.textContent);
+//if (block)  {
+//   console.log(block.textContent);
+//} console.log(undefined);
+
+//block?.textContent = '123'; // Syntacsis error
+
+//console.log(1 + 3);
+
+
+//const userData = {
+//   name: 'Vasek',
+//   age: null,
+//   say: function() {
+//      console.log('Hello');
+//   }
+//}
+
+//userData.say();
+//userData.hey?.();
+
+//if (userData && userData.skills && userData.skills.js) {
+//   console.log(userData.skills.js);
+//}
+//console.log(userData?.skills?.js);
+
+//=================================================================
