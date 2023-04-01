8fe319d77ecfb1b3ec1bf35593ab527a4ce7d84b
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-04-01 20:02:57 +0300

    Working with differents between HTMLCollection and psevdoarray

--- js/script.js
+++ js/script.js
@@ -59,3 +59,30 @@
 //console.log(userData?.skills?.js);
 
 //=================================================================
+
+//const boxesQuery = document.querySelectorAll('.box');
+//const boxesElement = document.getElementsByClassName('box');
+
+//boxesQuery.forEach(box => {
+//   if (box.matches('.this')) console.log(box);
+//});
+
+//console.log(boxesQuery[0].closest('.wrapper'));
+
+//boxesQuery[0].remove();
+//boxesElement[0].remove();
+
+//for (let i = 0; i < 5; i++) {
+//   const div = document.createElement('div');
+//   div.classList.add('box');
+//   document.body.append(div);
+//   //boxesElement[boxesElement.length] = div
+//}
+
+//console.log(boxesElement);
+//console.log(boxesQuery);
+////console.log(document.body.children);
+
+//console.log(Array.from(boxesElement));
+
+//=================================================================
