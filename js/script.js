b4506748704d56fa651eabf9d79acb4e7f5d3fa5
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-04-02 23:58:33 +0300

    Task about number of pages

--- js/script.js
+++ js/script.js
@@ -387,5 +387,30 @@
 //console.log(bigint + BigInt(number));
 //console.log(number + Number(bigint));
 
+//========================= TASK ========================================================
 
+function amountOfPages(summary){
+  //  if (summary < 10) {
+  //      return summary;
+  //  } else if (summary < 189) {
+  //      return (summary - 8);
+  //  } else { 
+  //    return (summary - 189) / 3; 
+  //}
+
+    let result = '';
+    let num = 0;
+    
+    for (let i = 1; i <= summary; i++) {
+        result += i;
+        if (result.length === summary) {
+            num = i;
+            break;
+        }
+    }
+    return num;
+
+}
+
+console.log(amountOfPages(25));
 
