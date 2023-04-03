56ae73e44a03dcffb3ebcf2497731a19ba0b5a75
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-04-03 21:57:45 +0300

    Did third task

--- js/script.js
+++ js/script.js
@@ -475,3 +475,21 @@
 //};
 
 //============= 3 task ===============================================
+
+// Вариант с циклом
+//function deepCount(a){
+//  let count = a.length;
+//  for (let i=0; i<a.length; i++) if (Array.isArray(a[i])) count += deepCount(a[i]);
+//  return count;
+//}
+
+//// Вариант с методом reduce
+//function deepCount(a){
+//  return a.reduce((s,e)=>s+(Array.isArray(e) ? deepCount(e) : 0),a.length);
+//}
+
+//console.log(deepCount([1, 2, [3, 4, [5]]]));
+
+//================================ classList ...........=========================
+
+
