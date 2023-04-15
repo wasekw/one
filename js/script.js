afec8158069b34892149355872c018307811b3a6
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-04-15 07:02:40 +0300

    Working with jsonplaceholder

---
+++ js/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// fetch('http://example.com/movies.json')
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+// Пример отправки POST запроса:
+// async function postData(url = '', data = {}) {
+//   // Default options are marked with *
+//   const response = await fetch(url, {
+//     method: 'POST', // *GET, POST, PUT, DELETE, etc.
+//     mode: 'cors', // no-cors, *cors, same-origin
+//     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+//     credentials: 'same-origin', // include, *same-origin, omit
+//     headers: {
+//       'Content-Type': 'application/json'
+//       // 'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//     redirect: 'follow', // manual, *follow, error
+//     referrerPolicy: 'no-referrer', // no-referrer, *client
+//     body: JSON.stringify(data) // body data type must match "Content-Type" header
+//   });
+//   return await response.json(); // parses JSON response into native JavaScript objects
+// }
+
+// postData('https://example.com/answer', { answer: 42 })
+//   .then((data) => {
+//     console.log(data); // JSON data parsed by `response.json()` call
+//   });
+
+// ===================  JSONPLACEHOLDER =====================================================
+
+fetch('https://jsonplaceholder.typicode.com/todos/2')
+      .then(response => response.json())
+      .then(json => console.log(json));
