2f86eb14fd9d292ab506fc3b9609e6d95f5682bd
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-04-19 05:45:20 +0300

    Ended working with some array method

--- js/script.js
+++ js/script.js
@@ -101,69 +101,75 @@
 
 // ==============  TASKS WITH METHOD FOR ARRAY ==============================================
 
-const films = [
-  {
-      name: 'Titanic',
-      rating: 9
-  },
-  {
-      name: 'Die hard 5',
-      rating: 5
-  },
-  {
-      name: 'Matrix',
-      rating: 8
-  },
-  {
-      name: 'Some bad film',
-      rating: 4
-  }
-];
+// const films = [
+//   {
+//       name: 'Titanic',
+//       rating: 9
+//   },
+//   {
+//       name: 'Die hard 5',
+//       rating: 5
+//   },
+//   {
+//       name: 'Matrix',
+//       rating: 8
+//   },
+//   {
+//       name: 'Some bad film',
+//       rating: 4
+//   }
+// ];
 
-function showGoodFilms(arr) {
-  return arr.filter(film => film.rating >= 8);
-}
+// function showGoodFilms(arr) {
+//   return arr.filter(film => film.rating >= 8);
+// }
 // console.log(showGoodFilms(films));
 
-function showListOfFilms(arr) {
+// function showListOfFilms(arr) {
   // return arr.reduce((acc, film) => acc += `${film.name}, `, '');
-  return arr.reduce((acc, curr) =>`${typeof(acc) === 'object' ? acc.name : acc}, ${curr.name}`);
-}
+//   return arr.reduce((acc, curr) =>`${typeof(acc) === 'object' ? acc.name : acc}, ${curr.name}`);
+// }
 // console.log(showListOfFilms(films));
 
-function setFilmsIds(arr) {
+// function setFilmsIds(arr) {
   // return arr.map(film => Object.entries(film));
-  return arr.map((film, ind) => {
-    film.id = ind;
-    return film;
-  });
-}
+  // return arr.map((film, ind) => {
+  //   film.id = ind;
+  //   return film;
+  // });
+// }
 // console.log(setFilmsIds(films));
 
 
-const tranformedArray = setFilmsIds(films);
+// const tranformedArray = setFilmsIds(films);
 
-function checkFilms(arr) {
-  return arr.every(film => film.id === 0 || film.id)
-}
+// function checkFilms(arr) {
+//   return arr.every(film => film.id === 0 || film.id)
+// }
 
-console.log(checkFilms(tranformedArray));
+// console.log(checkFilms(tranformedArray));
 
 // ================= SECOND PART ================================================
 
-const funds = [
-  {amount: -1400},
-  {amount: 2400},
-  {amount: -1000},
-  {amount: 500},
-  {amount: 10400},
-  {amount: -11400}
-];
+// const funds = [
+//   {amount: -1400},
+//   {amount: 2400},
+//   {amount: -1000},
+//   {amount: 500},
+//   {amount: 10400},
+//   {amount: -11400}
+// ];
 
-const getPositiveIncomeAmount = (data) => {
+// const getPositiveIncomeAmount = (data) => {
+//   return data.filter(el => el.amount > 0).reduce((acc, el) => acc += el.amount, 0);
+// };
+// // console.log(getPositiveIncomeAmount(funds));
 
-};
-
-const getTotalIncomeAmount = (data) => {
-
-};
+// const getTotalIncomeAmount = (data) => {
+//   if (data.some(el => el.amount < 0)) {
+//     return data.reduce((acc, el) => acc += el.amount, 0);
+//   } else {
+//     getPositiveIncomeAmount(data);
+//   }
+// };
+// console.log(getTotalIncomeAmount(funds));
