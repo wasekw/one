6148365b0607bac1fcdbe6c6180e150188c148a0
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-04-19 07:11:38 +0300

    Starting work with npm

--- js/script.js
+++ js/script.js
@@ -351,6 +351,10 @@ window.addEventListener('DOMContentLoaded', function() {
           closeModal();
         }, 4000)
       }
+
+      fetch('db.json')
+      .then(data => data.json())
+      .then(res => console.log(res));
 });
 
 
