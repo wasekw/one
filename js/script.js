9ecda4f27000de6170b2a698b8eff97ca7a22df3
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-05-24 13:30:46 +0300

    Ended workie with fetch

--- js/script.js
+++ js/script.js
@@ -252,11 +252,18 @@ window.addEventListener('DOMContentLoaded', function() {
       // };
 
 
-      getResource('http://localhost:3000/menu')
+      // getResource('http://localhost:3000/menu')
+      // .then(data => {
+      //   data.forEach(({img, altimg, title, descr, price}) => {
+      //     new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+      //   });
+      // });
+
+      axios.get('http://localhost:3000/menu')
       .then(data => {
-        data.forEach(({img, altimg, title, descr, price}) => {
-          new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
-        });
+        data.data.forEach(({img, altimg, title, descr, price}) => {
+        new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+      });
       });
 
       // new MenuCard(
